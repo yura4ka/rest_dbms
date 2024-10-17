a3072d21efa213feefc32f5d6fe75655d851888f
yura4ka <yura4ka05@gmail.com>
2024-10-17 18:48:40 +0300

    feat(client): setup theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,11 @@
+import { ThemeProvider } from "./components/theme-provider";
+
 function App() {
-  return <div className="text-3xl bg-primary text-primary-foreground p-6 text-center">Hello</div>;
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="text-3xl bg-primary text-primary-foreground p-6 text-center">Hello</div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
