139d37a51ad6373d2e8470bda2fb619c2887b273
yura4ka <yura4ka05@gmail.com>
2024-10-20 16:37:13 +0300

    feat(client): column panel

--- client/src/components/column-view.tsx
+++ client/src/components/column-view.tsx
@@ -7,8 +7,28 @@ type Props = {
 export function ColumnView({ column }: Props) {
   return (
     <div>
-      <h2>{column.name}</h2>
-      <p>{column.typeName}</p>
+      <table>
+        <tr className="py-2 text-lg">
+          <td className="pr-4">Name:</td>
+          <td>{column.name}</td>
+        </tr>
+        <tr className="py-2 text-lg">
+          <td className="pr-4">Type:</td>
+          <td>{column.typeName}</td>
+        </tr>
+        <tr className="py-2 text-lg">
+          <td className="pr-4">Primary key (pk):</td>
+          <td>{column.isPk ? "true" : "false"}</td>
+        </tr>
+        <tr className="py-2 text-lg">
+          <td className="pr-4">Not null (nn):</td>
+          <td>{column.isNotNull ? "true" : "false"}</td>
+        </tr>
+        <tr className="py-2 text-lg">
+          <td className="pr-4">Default value:</td>
+          <td>{column.defaultValue}</td>
+        </tr>
+      </table>
     </div>
   );
 }
